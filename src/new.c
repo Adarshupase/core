70a6bc4958d11a1fe1bb7ade3f7557f956bb3907
Adarsh Upase <adarshupase2121@gmail.com>
2026-01-29 02:42:30 +0530

    currently fields work fine need a work around for pointers HINT: Use field_expression for extracting all declarations

--- src/new.c
+++ src/new.c
@@ -8,10 +8,109 @@ typedef uint32_t u32;
 #define CURSOR_MODE 0
 #define VERBOSE 0
 #define DEBUG_VERBOSE 1
+#define MAX_DEFINION_SIZE 1000
 
+const TSLanguage *tree_sitter_c(void);
+char *array[MAX_DEFINION_SIZE] = {NULL};
+int array_size = 0;
+
+static void insert_into_array(const char *string) 
+{   char *dup_string = strndup(string,strlen(string));
+    if(array_size > MAX_DEFINION_SIZE-1) {
+        return;
+    }
+    array[array_size++] = dup_string;
+}
+
+static bool find_in_array(const char *string) 
+{
+    for(int i = 0; i < array_size; i++) {
+        if(strcmp(array[i],string) == 0) {
+            return true;
+        } 
+    }
+    return false;
+}
+static void cleanup()
+{
+    for(int i = 0; i < array_size; i++){
+        if(array[i] != NULL) {
+            free(array[i]);
+            array[i] = NULL;
+        }
+    }
+    array_size = 0;
+}
+
+static void find_all_struct_declarations(const char *struct_name, TSNode root_node,const char *modified_source) 
+{
+    String_Builder sb = {0};
+
+    sb_append(&sb, "(declaration type: (struct_specifier name: (type_identifier) @struct_name) ");
+    sb_append(&sb, "(#eq? @struct_name \"");
+    sb_append(&sb, struct_name);
+    sb_append(&sb, "\")) @struct_declaration");
+
+    const char *query_string = sb.string;
+    TSQueryError error_type; u32 error_offset;
+    TSQuery *query = ts_query_new(
+        tree_sitter_c(), 
+        query_string, 
+        strlen(query_string),
+        &error_offset,
+        &error_type
+    );
+    sb_free(&sb);
+
+
+    TSQueryCursor *cursor = ts_query_cursor_new();
+    TSQueryMatch match;
+
+    ts_query_cursor_exec(cursor, query, root_node);
+
+    while(ts_query_cursor_next_match(cursor,&match)) {
+        TSNode named_node = {0}; 
+
+        for(u32 i = 0; i < match.capture_count; i++) {
+            u32 length;
+            const char *cap = ts_query_capture_name_for_id(query,match.captures[i].index, &length);
+            if(strcmp(cap,"struct_declaration") == 0) {
+                TSNode node = match.captures[i].node;
+                named_node = find_child_node_of_type(node,"identifier");
+            }
+        }
+        if(!ts_node_is_null(named_node)){
+            u32 start_byte = ts_node_start_byte(named_node);
+            u32 end_byte   = ts_node_end_byte(named_node);
+            u32 slice_size = end_byte - start_byte;
+            
+            char *string = strndup(modified_source + start_byte, slice_size);
+            insert_into_array(string);
+            free(string);
+            string = NULL;
+        }
+    }
+
+}
 
 // definitions 
-const TSLanguage *tree_sitter_c(void);
+
+
+void sb_append(String_Builder *sb, const char *string) 
+{
+    int old_size = sb->size;
+    int new_size = old_size + strlen(string);
+    sb->string = realloc(sb->string, new_size + 1);
+    strcpy(sb->string + old_size, string);
+    sb->size = new_size;
+}
+
+void sb_free(String_Builder *sb) 
+{
+    free(sb->string);
+    sb->string = NULL;
+    sb->size = 0;
+}
 
 void traverse(TSNode root_node, int nest)
 {
@@ -91,10 +190,6 @@ void change_field_in_struct(char *modified,
                             TSInputEdit *edit)
 {
     
-
-    // let's just support 4 pointer because if you are using 5 pointer indirection 
-    // this refactoring engine is no help to you sorry for that
-    //@TODO(write a query generator that handles any number of pointers) NOT_URGENT
     const char *query_string = "(field_declaration) @field";
 
     // @TODO(make this as a macro or a function ) my code is already breaking i don't want to add more uncertainity
@@ -116,7 +211,7 @@ void change_field_in_struct(char *modified,
         TSNode name_node = {0};
         for(u32 i = 0; i < match.capture_count; i++) {
             TSNode node = match.captures[i].node;
-            name_node = find_identifier_node(node);
+            name_node = find_child_node_of_type(node, "field_identifier");
         }
         if(!ts_node_is_null(name_node)){
             u32 start_byte = ts_node_start_byte(name_node);
@@ -145,6 +240,7 @@ void change_field_in_struct(char *modified,
                     .row = start.row,
                     .column = start.column + new_slice_size
                 };
+                
 
                 break;
             } 
@@ -155,16 +251,88 @@ void change_field_in_struct(char *modified,
 
 }
 
-TSNode find_identifier_node(TSNode node)
+
+void change_struct_field_in_program(char *modified_source, const char *struct_name, const char *from, 
+                                    const char *to, TSInputEdit *edit, TSNode root_node)
+{
+
+    const char *query_string = "(field_expression argument: (identifier) @identifier_name field: (field_identifier) @field_name)";
+    
+    TSQueryError error_type; u32 error_offset;
+
+    TSQuery *query = ts_query_new(
+        tree_sitter_c(), 
+        query_string, 
+        strlen(query_string),
+        &error_offset,
+        &error_type
+    );
+    TSQueryCursor *cursor = ts_query_cursor_new();
+    TSQueryMatch match;
+
+    find_all_struct_declarations(struct_name, root_node, modified_source);
+    ts_query_cursor_exec(cursor, query, root_node);
+    while(ts_query_cursor_next_match(cursor, &match)){
+        TSNode identifier_node = {0};
+        TSNode field_node  = {0};
+        for(u32 i = 0; i < match.capture_count; i++){
+             u32 length;
+             const char *cap = ts_query_capture_name_for_id( query, match.captures[i].index, &length );
+             if ((strcmp(cap, "identifier_name") == 0))  identifier_node =  match.captures[i].node;
+             else if (strcmp(cap, "field_name") == 0) field_node = match.captures[i].node; 
+        }
+        if(!ts_node_is_null(identifier_node)) {
+            u32 start_byte = ts_node_start_byte(identifier_node);
+            u32 end_byte = ts_node_end_byte(identifier_node);
+            u32 slice_size = end_byte - start_byte;
+            u32 field_start_byte = ts_node_start_byte(field_node);
+            u32 field_end_byte = ts_node_end_byte(field_node);
+            u32 field_size = field_end_byte - field_start_byte;
+            char *string = strndup(modified_source + start_byte, slice_size);
+            if(find_in_array(string)) {
+                if (strncmp(modified_source + field_start_byte, from, field_size) == 0 && from[field_size] == '\0') 
+                { 
+                    size_t new_field_size = strlen(to);
+                    if(new_field_size != field_size) {
+                        memmove(
+                            modified_source + field_start_byte + new_field_size,
+                            modified_source + field_end_byte,   
+                            strlen(modified_source) - field_end_byte + 1
+                        );
+                    }
+                    memcpy(modified_source + field_start_byte, to, new_field_size); 
+                    edit->start_byte = field_start_byte;
+                    edit->old_end_byte = field_end_byte;
+                    edit->new_end_byte = field_start_byte + new_field_size;
+                    edit->start_point = ts_node_start_point(field_node);
+                    edit->old_end_point = ts_node_end_point(field_node);
+                    TSPoint start = ts_node_start_point(field_node);
+                    edit->new_end_point = (TSPoint){
+                        .row = start.row,
+                        .column = start.column + new_field_size
+                    };
+
+                }
+            }
+            free(string);
+            cleanup();
+
+        }
+    }
+    ts_query_cursor_delete(cursor);
+    ts_query_delete(query);
+
+}
+TSNode find_child_node_of_type(TSNode node, const char *type)
 {
     for (u32 i = 0; i < ts_node_child_count(node); i++) {
         TSNode child = ts_node_child(node, i);
 
-        if (strcmp(ts_node_type(child), "field_identifier") == 0) {
+        if (strcmp(ts_node_type(child), type) == 0) {
             return child;
         }
 
-        TSNode result = find_identifier_node(child);
+        TSNode result = find_child_node_of_type(child, type);
         if (!ts_node_is_null(result)) {
             return result;
         }
@@ -204,7 +372,6 @@ TSNode find_struct_with_name( const char *source, const char *struct_name, TSNod
             u32 end = ts_node_end_byte(name_node); 
             if (strncmp(source + start, struct_name, end - start) == 0 && struct_name[end - start] == '\0') 
             { 
-                
                 result = struct_node; 
                 break; 
             } 
@@ -234,6 +401,8 @@ void change_struct_field(const char *struct_name,
     TSInputEdit edit = {0};
     change_field_in_struct(modified_source, struct_node, from, to, &edit);
     ts_tree_edit(info->tree, &edit);
+    change_struct_field_in_program(modified_source,struct_name, from, to, &edit, root_node);
+    
     TSTree *new_tree = ts_parser_parse_string(
         info->parser, 
         info->tree,
@@ -318,13 +487,13 @@ int main(int argc, char *argv[])
     }
     
     
-    const TSLanguage *language = tree_sitter_c();
+    
 
 
     u32 error_offset ;
     TSQueryError error_type;
     TSQuery *query = ts_query_new(
-        language,
+        tree_sitter_c(),
         query_string,
         strlen(query_string),
         &error_offset,
@@ -338,7 +507,7 @@ int main(int argc, char *argv[])
     while(ts_query_cursor_next_match(query_cursor,&match)){
         for(u32 i= 0 ; i < match.capture_count; i++) {
             TSNode node = match.captures[i].node;
-            TSNode name_node = find_identifier_node(node);
+            TSNode name_node = find_child_node_of_type(node,"field_identifier");
             if(!ts_node_is_null(name_node)){
                 u32 start_byte = ts_node_start_byte(name_node);
                 u32 end_byte   = ts_node_end_byte(name_node);
